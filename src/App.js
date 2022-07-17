7445af08ae4e0ee1350063241bcc7871d4952730
meg151 <meg101810@gmail.com>
2022-07-17 21:45:22 +0900

    add Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Container } from './components/styles/Container.styled';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Container>
-      <h1>Hello World</h1>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </>
   );
 }
 
